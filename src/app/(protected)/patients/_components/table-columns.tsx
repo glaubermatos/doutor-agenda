c487a63ce1d18468ba3d387824c687bd1859e908
T37447 <glaub.oliveira@hotmail.com>
2025-06-04 15:58:38 -0300

    feat: add patients table

---
+++ src/app/(protected)/patients/_components/table-columns.tsx
@@ -0,0 +1,82 @@
+"use client";
+import { ColumnDef } from "@tanstack/react-table";
+import { EditIcon, MoreVerticalIcon, TrashIcon } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { patientsTable } from "@/db/schema";
+
+export type Patient = typeof patientsTable.$inferSelect;
+
+export const patientsTablecolumns: ColumnDef<Patient>[] = [
+  {
+    id: "name",
+    accessorKey: "name",
+    header: "Nome",
+  },
+  {
+    id: "email",
+    accessorKey: "email",
+    header: "E-mail",
+  },
+  {
+    id: "phoneNumber",
+    accessorKey: "phoneNumber",
+    header: "Telefone",
+    cell: (params) => {
+      const patient = params.row.original;
+      const phoneNumber = patient.phoneNumber;
+
+      if (!phoneNumber) return "";
+
+      const formatted = phoneNumber.replace(
+        /(\d{2})(\d{5})(\d{4})/,
+        "($1) $2-$3",
+      );
+      return formatted;
+    },
+  },
+  {
+    id: "sex",
+    accessorKey: "sex",
+    header: "Sexo",
+    cell: (params) => {
+      const patient = params.row.original;
+      return patient.sex === "male" ? "Masculino" : "Feminino";
+    },
+  },
+  {
+    id: "actions",
+    cell: (params) => {
+      const patient = params.row.original;
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant={"ghost"} size={"icon"}>
+              <MoreVerticalIcon className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>{patient.name}</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <EditIcon />
+              Editar
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <TrashIcon />
+              Deletar
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
--- src/app/(protected)/patients/page.tsx
+++ src/app/(protected)/patients/page.tsx
@@ -1,15 +1,41 @@
+import { eq } from "drizzle-orm";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
 import {
   PageActions,
   PageContainer,
+  PageContent,
   PageDescription,
   PageHeader,
   PageHeaderContent,
   PageTitle,
 } from "@/components/ui/page-container";
+import { db } from "@/db";
+import { patientsTable } from "@/db/schema";
+import { auth } from "@/lib/auth";
 
+import { DataTable } from "../../../components/ui/data-table";
 import { AddPatientButton } from "./_components/add-patient-button";
+import { patientsTablecolumns } from "./_components/table-columns";
 
-const PatientsPage = () => {
+const PatientsPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session?.user) {
+    redirect("/authentication");
+  }
+
+  if (!session.user.clinic) {
+    redirect("/clinic-form");
+  }
+
+  const patients = await db.query.patientsTable.findMany({
+    where: eq(patientsTable.clinicId, session.user.clinic.id),
+  });
+
   return (
     <PageContainer>
       <PageHeader>
@@ -24,6 +50,10 @@ const PatientsPage = () => {
           <AddPatientButton />
         </PageActions>
       </PageHeader>
+
+      <PageContent>
+        <DataTable columns={patientsTablecolumns} data={patients} />
+      </PageContent>
     </PageContainer>
   );
 };
